3e93d54d20d24d1214b4c44eb2ca415e4284ea4b
LorellW <lorellwendel@gmail.com>
2023-04-06 21:47:38 +0300

    Done

--- src/main/java/com/github/lorellw/someFun/Solution.java
+++ src/main/java/com/github/lorellw/someFun/Solution.java
@@ -1,5 +1,33 @@
 package com.github.lorellw.someFun;
 
 public class Solution {
-    
+
+    /*
+    The function must accept a non-negative integer.
+    If it is zero, it just returns "now".
+    Otherwise, the duration is expressed as a combination of years, days, hours, minutes and seconds.
+     */
+
+    public static String formatDuration(int seconds) {
+        if (seconds == 0) {
+            return "now";
+        }
+        StringBuilder result = new StringBuilder("");
+        String[] durations = new String[]{"year", "day", "hour", "minute", "second"};
+        int[] temp = new int[]{31536000, 86400, 3600, 60, 1};
+        int quantity;
+        for (int i = 0; i < 5; i++) {
+            if (seconds >= temp[i]) {
+                quantity = seconds / temp[i];
+                seconds %= temp[i];
+                result.append(result.toString().equals("") ? "" : (seconds == 0 ? " and " : ", "))
+                        .append(quantity)
+                        .append(" ")
+                        .append(durations[i])
+                        .append(quantity > 1 ? "s" : "");
+            }
+        }
+
+        return result.toString();
+    }
 }
--- src/test/java/com/github/lorellw/someFun/SolutionTest.java
+++ src/test/java/com/github/lorellw/someFun/SolutionTest.java
@@ -1,7 +1,17 @@
 package com.github.lorellw.someFun;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
-class SolutionTest {
+public class SolutionTest {
 
+    @Test
+    public void formatDurationTest(){
+
+        assertEquals("1 second", Solution.formatDuration(1));
+        assertEquals("1 minute and 2 seconds", Solution.formatDuration(62));
+        assertEquals("2 minutes", Solution.formatDuration(120));
+        assertEquals("1 hour", Solution.formatDuration(3600));
+        assertEquals("1 hour, 1 minute and 2 seconds", Solution.formatDuration(3662));
+    }
 }
