1cf15aa627cf5b1b12ad3a8ec58a2e832a154258
LorellW <lorellwendel@gmail.com>
2023-04-11 11:47:33 +0300

    doen

--- src/main/java/com/github/lorellw/someFun/Solution.java
+++ src/main/java/com/github/lorellw/someFun/Solution.java
@@ -1,5 +1,45 @@
 package com.github.lorellw.someFun;
 
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Queue;
+
 public class Solution {
-    
+
+    /*
+        Given an n x n array, return the array elements arranged from outermost elements to the middle element, traveling clockwise.
+        int[][] array
+                = {{1, 2, 3, 4, 5},
+                {6, 7, 8, 9, 10},
+                {11, 12, 13, 14, 15},
+                {16, 17, 18, 19, 20},
+                {21, 22, 23, 24, 25}};
+        int[] r = {1, 2, 3, 4, 5, 10, 15, 20, 25, 24, 23, 22, 21, 16, 11, 6, 7, 8, 9, 14, 19, 18, 17, 12, 13};
+     */
+
+    public static int[] snail(int[][] array) {
+        array = new int[0][0];
+        int n = array.length;
+        int n2 = array[0].length;
+        Queue<Integer> queue = new ArrayDeque<>();
+        for (int j = 0; j < n / 2 + 1; j++) {
+            for (int i = j; i < n - j; i++) {
+                queue.add(array[j][i]);
+            }
+            for (int i = j + 1; i < n - j; i++) {
+                queue.add(array[i][n - j - 1]);
+            }
+            for (int i = n - j - 2; i >= j; i--) {
+                queue.add(array[n - j - 1][i]);
+            }
+            for (int i = n - j - 2; i >= j + 1; i--) {
+                queue.add(array[i][j]);
+            }
+        }
+        int[] result = new int[n * n];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = queue.remove();
+        }
+        return result;
+    }
 }
--- src/test/java/com/github/lorellw/someFun/SolutionTest.java
+++ src/test/java/com/github/lorellw/someFun/SolutionTest.java
@@ -1,7 +1,34 @@
 package com.github.lorellw.someFun;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static java.util.stream.Collectors.joining;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class SolutionTest {
 
+    @Test
+    public void SnailTest1() {
+        int[][] array
+                = {{1, 2, 3, 4, 5},
+                {6, 7, 8, 9, 10},
+                {11, 12, 13, 14, 15},
+                {16, 17, 18, 19, 20},
+                {21, 22, 23, 24, 25}};
+        int[] r = {1, 2, 3, 4, 5, 10, 15, 20, 25, 24, 23, 22, 21, 16, 11, 6, 7, 8, 9, 14, 19, 18, 17, 12, 13};
+        test(array, r);
+    }
+
+    public String int2dToString(int[][] a) {
+        return Arrays.stream(a).map(row -> Arrays.toString(row)).collect(joining("\n"));
+    }
+
+    public void test(int[][] array, int[] result) {
+        String text = int2dToString(array) + " should be sorted to " + Arrays.toString(result);
+        System.out.println(text);
+        Assert.assertArrayEquals(result, Solution.snail(array));
+    }
 }
