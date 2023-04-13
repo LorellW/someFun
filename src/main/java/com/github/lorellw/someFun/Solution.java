484e37a2bfef98ec2f0fede844a3d4230c595883
LorellW <lorellwendel@gmail.com>
2023-04-13 20:55:27 +0300

    done

--- src/main/java/com/github/lorellw/someFun/Solution.java
+++ src/main/java/com/github/lorellw/someFun/Solution.java
@@ -1,5 +1,19 @@
 package com.github.lorellw.someFun;
 
 public class Solution {
-    
+
+    public static String stripComments(String text, String[] commentSymbols) {
+        String[] splitString = text.split("\n");
+        StringBuilder result = new StringBuilder("");
+        for (String str: splitString) {
+            for (String deleter: commentSymbols) {
+                if (str.contains(deleter)){
+                    str = str.substring(0,str.indexOf(deleter));
+                }
+            }
+            result.append(str.stripTrailing()).append("\n");
+        }
+        result.setLength(result.length()-1);
+        return result.toString();
+    }
 }
--- src/test/java/com/github/lorellw/someFun/SolutionTest.java
+++ src/test/java/com/github/lorellw/someFun/SolutionTest.java
@@ -1,7 +1,22 @@
 package com.github.lorellw.someFun;
 
+import org.junit.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class SolutionTest {
+    @Test
+    public void stripComments() throws Exception {
+        assertEquals(
+                "apples, pears\ngrapes\nbananas",
+                Solution.stripComments( "apples, pears # and bananas\ngrapes\nbananas !apples", new String[] { "#", "!" } )
+        );
+
+        assertEquals(
+                "a\nc\nd",
+                Solution.stripComments( "a #b\nc\nd $e f g", new String[] { "#", "$" } )
+        );
+
+    }
 
 }
