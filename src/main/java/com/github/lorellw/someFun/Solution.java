9a096ff881d512a3e5c1b3c9149701b1a3f76b67
LorellW <lorellwendel@gmail.com>
2023-04-17 20:49:59 +0300

    done

--- src/main/java/com/github/lorellw/someFun/Solution.java
+++ src/main/java/com/github/lorellw/someFun/Solution.java
@@ -1,5 +1,45 @@
 package com.github.lorellw.someFun;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Solution {
-    
+
+    private static List<String> strings;
+
+    public static List<String> singlePermutations(String str) {
+        strings = new ArrayList<>();
+        permutations(str.split(""),str.length());
+        return strings;
+    }
+
+    private static void permutations(String[] arr, int size) {
+        if (size == 1) {
+            if (!strings.contains(arrToString(arr))) {
+                strings.add(arrToString(arr));
+            }
+        }
+        for (int i = 0; i < size; i++) {
+            permutations(arr, size - 1);
+            if (size % 2 == 0) {
+                swap(arr, 0, size - 1);
+            } else {
+                swap(arr, i, size - 1);
+            }
+        }
+    }
+
+    private static void swap(String[] arr, int i, int j) {
+        String temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    private static String arrToString(String[] arr) {
+        StringBuilder builder = new StringBuilder();
+        for (String s : arr) {
+            builder.append(s);
+        }
+        return builder.toString();
+    }
 }
--- src/test/java/com/github/lorellw/someFun/SolutionTest.java
+++ src/test/java/com/github/lorellw/someFun/SolutionTest.java
@@ -1,7 +1,35 @@
 package com.github.lorellw.someFun;
 
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class SolutionTest {
 
+    @Test public void example1() {
+        assertEquals( new ArrayList<String>(Arrays.asList("a")),
+                Solution.singlePermutations("a").stream().sorted().collect(Collectors.toList()) );
+    }
+
+    @Test public void example2() {
+        assertEquals( new ArrayList<String>(Arrays.asList("ab","ba")),
+                Solution.singlePermutations("ab").stream().sorted().collect(Collectors.toList()) );
+    }
+
+    @Test
+    public void example3() {
+        assertEquals( new ArrayList<String>(Arrays.asList("aabb", "abab", "abba", "baab", "baba", "bbaa")),
+                Solution.singlePermutations("aabb").stream().sorted().collect(Collectors.toList()) );
+    }
+    @Test
+    public void example4() {
+        assertEquals( new ArrayList<String>(Arrays.asList("hqst", "hqts", "hsqt", "hstq", "htqs", "htsq",
+                "qhst", "qhts", "qsht", "qsth", "qths", "qtsh", "shqt", "shtq", "sqht",
+                "sqth", "sthq", "stqh", "thqs", "thsq", "tqhs", "tqsh", "tshq", "tsqh")),
+                Solution.singlePermutations("hqst").stream().sorted().collect(Collectors.toList()) );
+    }
 }
