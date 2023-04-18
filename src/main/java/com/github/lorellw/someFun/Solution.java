0ac9847139cc5feb9fb30c3e565a618d5426ae38
LorellW <lorellwendel@gmail.com>
2023-04-18 15:42:47 +0300

    done

--- src/main/java/com/github/lorellw/someFun/Solution.java
+++ src/main/java/com/github/lorellw/someFun/Solution.java
@@ -1,5 +1,48 @@
 package com.github.lorellw.someFun;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
 public class Solution {
-    
+
+    private static Map<String, Integer> romanNumbers = new HashMap<>(){
+        {
+            put("I",1);
+            put("V",5);
+            put("X",10);
+            put("L",50);
+            put("C",100);
+            put("D",500);
+            put("M",1000);
+        }
+    };
+
+    public static String toRoman(int i) {
+        return "I".repeat(i)
+                .replace("IIIII", "V")
+                .replace("IIII","IV")
+                .replace("VV","X")
+                .replace("VIV","IX")
+                .replace("XXXXX","L")
+                .replace("XXXX","XL")
+                .replace("LL","C")
+                .replace("LXL","XC")
+                .replace("CCCCC","D")
+                .replace("CCCC","CD")
+                .replace("DD","M")
+                .replace("DCD","CM");
+    }
+
+    public static int fromRoman(String i) {
+        String[] numbers = i.split("");
+        Stack<Integer> temp = new Stack<>();
+        for (String s: numbers) {
+            if (!temp.isEmpty() && temp.peek() < romanNumbers.get(s)) {
+                temp.push(temp.pop() * (-1));
+            }
+            temp.push(romanNumbers.get(s));
+        }
+        return temp.stream().mapToInt(integer -> integer).sum();
+    }
 }
--- src/test/java/com/github/lorellw/someFun/SolutionTest.java
+++ src/test/java/com/github/lorellw/someFun/SolutionTest.java
@@ -1,9 +1,24 @@
 package com.github.lorellw.someFun;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.Test;
 
 public class SolutionTest {
+    @Test
+    public void testToRoman() throws Exception {
+        assertThat("1 converts to 'I'", Solution.toRoman(1), is("I"));
+        assertThat("2 converts to 'II'", Solution.toRoman(2), is("II"));
+        assertThat("1456 converts to 'MCDLVI'", Solution.toRoman(1456), is("MCDLVI"));
+    }
+
+    @Test
+    public void testFromRoman() throws Exception {
+        assertThat("'I' converts to 1", Solution.fromRoman("I"), is(1));
+        assertThat("'II' converts to 2", Solution.fromRoman("II"), is(2));
+        assertThat("'MCDLVI' convert to 1456",Solution.fromRoman("MCDLVI"), is(1456));
+    }
 
 }
