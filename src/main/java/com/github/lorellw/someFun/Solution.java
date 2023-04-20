f1097326f5df5fe0cde5a774b9b46310b8f9ec95
LorellW <lorellwendel@gmail.com>
2023-04-20 18:49:05 +0300

    done

--- src/main/java/com/github/lorellw/someFun/Solution.java
+++ src/main/java/com/github/lorellw/someFun/Solution.java
@@ -1,5 +1,29 @@
 package com.github.lorellw.someFun;
 
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Solution {
-    
+
+    public static List<String> top3(String string) {
+
+        Map<String, Integer> wordsMap = new HashMap<>();
+        Arrays.stream(string.toLowerCase().split("[^a-z0-9']")).forEach(s -> {
+            if (!s.equals("") && !s.matches("'+")) {
+                if (wordsMap.containsKey(s)) {
+                    wordsMap.put(s, wordsMap.get(s) + 1);
+                } else wordsMap.put(s, 1);
+            }
+        });
+
+        return wordsMap.entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .limit(3)
+                .collect(Collectors.toMap( Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new))
+                .keySet()
+                .stream()
+                .toList();
+    }
 }
--- src/test/java/com/github/lorellw/someFun/SolutionTest.java
+++ src/test/java/com/github/lorellw/someFun/SolutionTest.java
@@ -4,6 +4,28 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.Test;
 
-public class SolutionTest {
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+public class SolutionTest {
+    @Test
+    public void sampleTests() {
+        assertEquals(Arrays.asList("e", "d", "a"), Solution.top3("a a a  b  c c  d d d d  e e e e e"));
+        assertEquals(Arrays.asList("e", "ddd", "aa"), Solution.top3("e e e e DDD ddd DdD: ddd ddd aa aA Aa, bb cc cC e e e"));
+        assertEquals(Arrays.asList("won't", "wont"), Solution.top3("  //wont won't won't "));
+        assertEquals(Arrays.asList("e"), Solution.top3("  , e   .. "));
+        assertEquals(Arrays.asList(), Solution.top3("  ...  "));
+        assertEquals(Arrays.asList(), Solution.top3("  '  "));
+        assertEquals(Arrays.asList(), Solution.top3("  '''  "));
+        assertEquals(Arrays.asList("jhukqba"),Solution.top3("jhukqba, jhukqba_jhukqba"));
+        assertEquals(Arrays.asList("a", "of", "on"), Solution.top3(Stream
+                .of("In a village of La Mancha, the name of which I have no desire to call to",
+                        "mind, there lived not long since one of those gentlemen that keep a lance",
+                        "in the lance-rack, an old buckler, a lean hack, and a greyhound for",
+                        "coursing. An olla of rather more beef than mutton, a salad on most",
+                        "nights, scraps on Saturdays, lentils on Fridays, and a pigeon or so extra",
+                        "on Sundays, made away with three-quarters of his income.")
+                .collect(Collectors.joining("\n"))));
+    }
 }
