6c10c2bac6edfed709563144542bdb5ea5a74ab8
LorellW <lorellwendel@gmail.com>
2023-05-12 16:07:03 +0300

    done

--- src/main/java/com/github/lorellw/someFun/Solution.java
+++ src/main/java/com/github/lorellw/someFun/Solution.java
@@ -1,5 +1,27 @@
 package com.github.lorellw.someFun;
 
 public class Solution {
-    
+    public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int[] arr = new int[nums1.length + nums2.length];
+        int j = 0, k = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (k >= nums2.length ||( j < nums1.length && nums1[j] < nums2[k])) {
+                arr[i] = nums1[j];
+                j++;
+            } else {
+                arr[i] = nums2[k];
+                k++;
+            }
+        }
+        return findMedian(arr);
+    }
+
+    private static double findMedian(int[] arr) {
+        if (arr.length % 2 == 0) {
+            return (arr[arr.length / 2 - 1] + arr[arr.length / 2]) / 2.0;
+        } else {
+            return arr[arr.length / 2];
+        }
+    }
+
 }
--- src/test/java/com/github/lorellw/someFun/SolutionTest.java
+++ src/test/java/com/github/lorellw/someFun/SolutionTest.java
@@ -5,5 +5,9 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.Test;
 
 public class SolutionTest {
-
+    @Test
+    public void findMedianSortedArraysTest() {
+        assertEquals(2.50,Solution.findMedianSortedArrays(new int[]{1,2},new int[]{3,4}));
+        assertEquals(2.00,Solution.findMedianSortedArrays(new int[]{1,3},new int[]{2}));
+    }
 }
