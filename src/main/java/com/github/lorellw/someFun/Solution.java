6e78a0c74883c950fbe1efafff940291a780bdf2
LorellW <lorellwendel@gmail.com>
2023-05-12 15:03:19 +0300

    done

--- src/main/java/com/github/lorellw/someFun/Solution.java
+++ src/main/java/com/github/lorellw/someFun/Solution.java
@@ -1,5 +1,25 @@
 package com.github.lorellw.someFun;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Solution {
-    
+    public static int lengthOfLongestSubstring(String s) {
+        char[] arr = s.toCharArray();
+        Set<Character> set = new HashSet<>();
+        int length = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr.length; j++) {
+                if (!set.contains(arr[j])){
+                    set.add(arr[j]);
+                }else{
+                    break;
+                }
+            }
+            length = Math.max(length, set.size());
+            set.clear();
+        }
+        return length;
+    }
 }
--- src/test/java/com/github/lorellw/someFun/SolutionTest.java
+++ src/test/java/com/github/lorellw/someFun/SolutionTest.java
@@ -5,5 +5,11 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.Test;
 
 public class SolutionTest {
-
+    @Test
+    public void lengthOfLongestSubstringTest() {
+        assertEquals(3,Solution.lengthOfLongestSubstring("abcabcabc"));
+        assertEquals(1,Solution.lengthOfLongestSubstring("bbbbbbbb"));
+        assertEquals(3,Solution.lengthOfLongestSubstring("pwwkew"));
+        assertEquals(1,Solution.lengthOfLongestSubstring("r"));
+    }
 }
