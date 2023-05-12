981064fd2663e9db34b132e0ef416794bc7320e2
LorellW <lorellwendel@gmail.com>
2023-05-12 08:31:54 +0300

    done

--- src/test/java/com/github/lorellw/someFun/SolutionTest.java
+++ src/test/java/com/github/lorellw/someFun/SolutionTest.java
@@ -1,9 +1,13 @@
 package com.github.lorellw.someFun;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class SolutionTest {
 
+    @Test
+    void twoSum() {
+        assertArrayEquals(new int[]{1,2},Solution.twoSum(new int[]{3,2,4},6));
+    }
 }
