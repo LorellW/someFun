82ff55dd4278af0603b9caf51ee215f2f1e95b42
LorellW <lorellwendel@gmail.com>
2023-05-19 07:42:40 +0300

    done

--- src/main/java/com/github/lorellw/someFun/Solution.java
+++ src/main/java/com/github/lorellw/someFun/Solution.java
@@ -1,5 +1,18 @@
 package com.github.lorellw.someFun;
 
 public class Solution {
-    
+
+    public static boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+        int y = 0, x2 = x;
+        while (x2 > 0) {
+            y *= 10;
+            y += (x2 % 10);
+            x2 /= 10;
+        }
+
+        return x == y;
+    }
 }
--- src/test/java/com/github/lorellw/someFun/SolutionTest.java
+++ src/test/java/com/github/lorellw/someFun/SolutionTest.java
@@ -5,5 +5,12 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.Test;
 
 public class SolutionTest {
-
+    @Test
+    public void isPalindromeTest(){
+        assertTrue(Solution.isPalindrome(121));
+        assertFalse(Solution.isPalindrome(-121));
+        assertFalse(Solution.isPalindrome(123));
+        assertTrue(Solution.isPalindrome(4));
+        assertTrue(Solution.isPalindrome(123454321));
+    }
 }
