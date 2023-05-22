fbad36aed88d10f9f9d1ab34e197a3e2c63f90cd
LorellW <lorellwendel@gmail.com>
2023-05-22 14:20:33 +0300

    done

--- src/main/java/com/github/lorellw/someFun/Solution.java
+++ src/main/java/com/github/lorellw/someFun/Solution.java
@@ -3,4 +3,24 @@ package com.github.lorellw.someFun;
 
 public class Solution {
 
+    public static String longestCommonPrefix(String[] strings) {
+        StringBuilder prefix = new StringBuilder();
+        if (strings == null || strings.length == 0){
+            return prefix.toString();
+        }
+        int minLength = strings[0].length();
+        for (int i = 1; i < strings.length; i++) {
+            minLength = Math.min(minLength,strings[i].length());
+        }
+        for (int i = 0; i < minLength; i++) {
+            char currentChar = strings[0].charAt(i);
+            for (String str: strings) {
+                if (str.charAt(i) != currentChar){
+                    return prefix.toString();
+                }
+            }
+            prefix.append(currentChar);
+        }
+        return prefix.toString();
+    }
 }
--- src/test/java/com/github/lorellw/someFun/SolutionTest.java
+++ src/test/java/com/github/lorellw/someFun/SolutionTest.java
@@ -6,5 +6,11 @@ import org.junit.Test;
 
 
 public class SolutionTest {
+    @Test
+    public void longestCommonPrefixTest(){
+        assertEquals("fl",Solution.longestCommonPrefix(new String[]{"flower","flow","flight"}));
+        assertEquals("",Solution.longestCommonPrefix(new String[]{"dog","racecar","car"}));
+        assertEquals("",Solution.longestCommonPrefix(null));
+    }
 
 }
